9d0a3910cffd61980ce91d68b0d4eedb05ddb7ac
JuanOssa1 <juan.ossa@hotmail.com>
2022-10-16 09:27:37 -0500

    Navigation to auth added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Navbar, Button } from 'reactstrap';
 import MemePage from './pages/MemePage'; 
 import FavoritesPage from './pages/FavoritesPage';
+import { AuthPage } from './pages/AuthPage';
 
 function App() {
+  const navigate = useNavigate();
+  const navigateAuth = () => {
+
+    navigate('/auth');
+  };
   return (
     <div className="App">
       <Navbar>
         <Button color='primary'>Home</Button> 
-        <Button color='danger'>Authenticate</Button> 
+        <Button onClick={navigateAuth} color='danger'>Authenticate</Button> 
       </Navbar>
       <h1 href='/' style={{cursor: 'pointer'}}>Meme Website</h1>
       <Routes>
         <Route path='/' element={<MemePage />} />
         <Route path='/favorites' element={<FavoritesPage/>} />
+        <Route path='/auth' element={<AuthPage/>}></Route>
       </Routes>
     </div>
   );
