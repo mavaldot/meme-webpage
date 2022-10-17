13c4190fd5bc8996858ccee307487832a242f6a7
JuanOssa1 <juan.ossa@hotmail.com>
2022-10-17 11:13:19 -0500

    Alerta de contraseña agregada

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -11,108 +11,107 @@ import { UserAuth } from "../context/AuthContext";
 
 const theme = createTheme();
 
-
-
 const AuthPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-    const { signIn, register} = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const { signIn, register } = UserAuth();
 
-    const handleSignIn = async (e) => {
-        e.preventDefault();
-        setError('')
-        try {
-            await signIn(email, password);
-            navigate('/memes')
-        } catch (error) {
-            setError(error.message)
-            console.log(error.message)
-        }
-
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signIn(email, password);
+      navigate("/memes");
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
     }
+  };
 
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        setError('')
-        try {
-            await register(email, password);
-            navigate('/memes')
-        } catch (error) {
-            setError(error.message)
-            console.log(error.message)
-        }
-
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await register(email, password);
+      navigate("/memes");
+    } catch (error) {
+      setError(error.message);
+      console.log(error.message);
     }
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Iniciar Sesion
-                    </Typography>
-                    <Box
-                        component="form"
-                        //onSubmit={handleSubmit}
-                        noValidate
-                        sx={{ mt: 1 }}
-                    >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Correo Electronico"
-                            name="email"
-                            autoComplete="email"
-                            onChange={(event) => setEmail(event.target.value)}
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Contraseña"
-                            type="password"
-                            id="password"
-                            onChange={(event) => setPassword(event.target.value)}
-                            autoComplete="current-password"
-                        />
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Iniciar Sesion
+          </Typography>
+          <Box
+            component="form"
+            //onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Correo Electronico"
+              name="email"
+              autoComplete="email"
+              onChange={(event) => setEmail(event.target.value)}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="password"
+              onChange={(event) => setPassword(event.target.value)}
+              autoComplete="current-password"
+            />
 
-                        <Button
-                            onClick={handleSignIn}
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Iniciar Sesion
-                        </Button>
-                        <Button
-                            onClick={handleRegister}
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Registrarme
-                        </Button>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-    );
+            <Button
+              onClick={handleSignIn}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Iniciar Sesion
+            </Button>
+            <Typography component="h10" variant="h10">
+              La Contraseña debe ser de minimo 6 caracteres
+            </Typography>
+            <Button
+              onClick={handleRegister}
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Registrarme
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default AuthPage;
