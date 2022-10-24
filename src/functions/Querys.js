a7e786f79b75cec94484c667a66bf0f43c070da3
JuanOssa1 <juan.ossa2@u.icesi.edu.co>
2022-10-24 02:27:48 -0500

    consultas agregadas solicitando id de usuario

--- src/functions/Querys.js
+++ src/functions/Querys.js
@@ -7,59 +7,46 @@ const auth = getAuth();
 
 
 
-export const getFavoriteMeme = async (memeId) => {
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const docSnap = await getDoc(doc(db, "user", user.uid))
-            let userValues = JSON.parse(docSnap)
-            const memeArrayPosition = userValues.memes.findIndex(x=>x.memeId == memeId)
-            return userValues.memes[memeArrayPosition]
-        }else {
 
-        }
-    });
+
+
+
+
+
+
+
+
+
+
+export const getFavoriteMeme = async (userId, memeId) => {
+    const docSnap = await getDoc(doc(db, "user", userId))
+    let userValues = JSON.parse(docSnap)
+    const memeArrayPosition = userValues.memes.findIndex(x => x.memeId == memeId)
+    return userValues.memes[memeArrayPosition]
 }
 
-export const addFavoriteMeme = async (memeId, memeToAdd, comment) => {
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const docSnap = await getDoc(doc(db, "user", user.uid))
-            let userValues = JSON.parse(docSnap)
-            userValues.memes.push({memeId: memeId, memeFavorite: memeToAdd, memeComment:comment})
-            const userValuesUpdatedJSON = JSON.stringify(userValues)
-            await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
-        }else {
-
-        }
-    });
+export const addFavoriteMeme = async (userId, memeId, memeToAdd, comment) => {
+    const docSnap = await getDoc(doc(db, "user", userId))
+    let userValues = JSON.parse(docSnap)
+    userValues.memes.push({ memeId: memeId, memeFavorite: memeToAdd, memeComment: comment })
+    const userValuesUpdatedJSON = JSON.stringify(userValues)
+    await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
 }
-export const deleteFavoriteMeme = async (memeId) => {
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const docSnap = await getDoc(doc(db, "user", user.uid))
-            let userValues = JSON.parse(docSnap)
-            const memeArrayPosition = userValues.memes.findIndex(x=>x.memeId == memeId)
-            userValues.memes.splice(memeArrayPosition, 1)
-            const userValuesUpdatedJSON = JSON.stringify(userValues)
-            await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
-        }else {
+export const deleteFavoriteMeme = async (userId, memeId) => {
+    const docSnap = await getDoc(doc(db, "user", userId))
+    let userValues = JSON.parse(docSnap)
+    const memeArrayPosition = userValues.memes.findIndex(x => x.memeId == memeId)
+    userValues.memes.splice(memeArrayPosition, 1)
+    const userValuesUpdatedJSON = JSON.stringify(userValues)
+    await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
 
-        }
-    });
 }
-export const updateFavoriteMemeComment = async (memeId, newComment) => {
-    onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const docSnap = await getDoc(doc(db, "user", user.uid))
-            let userValues = JSON.parse(docSnap)
-            const memeArrayPosition = userValues.memes.findIndex(x=>x.memeId == memeId)
-            userValues.memes[memeArrayPosition].memeComment = newComment;
-            const userValuesUpdatedJSON = JSON.stringify(userValues)
-            await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
-        }else {
-
-        }
-    });
-
+export const updateFavoriteMemeComment = async (userId,memeId, newComment) => {
+    const docSnap = await getDoc(doc(db, "user", userId))
+    let userValues = JSON.parse(docSnap)
+    const memeArrayPosition = userValues.memes.findIndex(x => x.memeId == memeId)
+    userValues.memes[memeArrayPosition].memeComment = newComment;
+    const userValuesUpdatedJSON = JSON.stringify(userValues)
+    await setDoc(doc(db, "user", uid), userValuesUpdatedJSON);
 }
 
